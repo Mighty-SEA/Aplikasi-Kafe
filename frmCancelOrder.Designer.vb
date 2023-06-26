ba461fd2de83d563a40257c18e1488ae938f0bf3
Mighty-SEA <hibban.habiburrahman@outlook.com>
2023-06-26 10:40:06 +0700

    Tambah Cancel Order

---
+++ frmCancelOrder.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCancelOrder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewButtonColumn()
+        txt_search = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3})
+        DataGridView1.Location = New Point(14, 47)
+        DataGridView1.Margin = New Padding(4, 3, 4, 3)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(905, 458)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "OrderNo"
+        Column1.Name = "Column1"
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Total Price"
+        Column2.Name = "Column2"
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Action"
+        Column3.Name = "Column3"
+        Column3.Text = ""
+        ' 
+        ' txt_search
+        ' 
+        txt_search.Location = New Point(14, 14)
+        txt_search.Margin = New Padding(4, 3, 4, 3)
+        txt_search.Name = "txt_search"
+        txt_search.Size = New Size(905, 23)
+        txt_search.TabIndex = 1
+        ' 
+        ' frmCancelOrder
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(933, 519)
+        Controls.Add(txt_search)
+        Controls.Add(DataGridView1)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "frmCancelOrder"
+        Text = "Cancel Order"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txt_search As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewButtonColumn
+End Class
---
+++ frmCancelOrder.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+Public Class frmCancelOrder
+    Dim conn As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & Application.StartupPath & "\coffee.accdb")
+    Dim dr As OleDbDataReader
+    Dim i As Integer
+
+    Dim btncategory As New Button
+    Private Sub frmCancelOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        orderload()
+    End Sub
+
+    Sub orderload()
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New OleDb.OleDbCommand("SELECT sum(`price`) as `price`,`orderno` FROM OrderMaster group by orderno", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("orderno"), dr.Item("price"))
+
+            End While
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New OleDb.OleDbCommand("SELECT sum(`price`) as `price`,`orderno` FROM OrderMaster where orderno like '%" & txt_search.Text & "%' group by orderno ", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("orderno"), dr.Item("price"))
+
+            End While
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If MsgBox("Are You Sure Cancel this Order", MsgBoxStyle.Information + vbYesNo) = vbYes Then
+            Dim colname As String = DataGridView1.Columns(e.ColumnIndex).Name
+            If colname = "Column2" Then
+                conn.Open()
+                Dim cmd As New OleDb.OleDbCommand("DELETE FROM `OrderMaster` WHERE orderno=@orderno", conn)
+                cmd.Parameters.Clear()
+                cmd.Parameters.AddWithValue("@orderno", DataGridView1.CurrentRow.Cells(0).Value.ToString)
+                i = cmd.ExecuteNonQuery
+                If i > 0 Then
+                    MsgBox("Order Cancel Successfully")
+
+                End If
+            End If
+            conn.Close()
+        Else
+            Return
+        End If
+        orderload()
+    End Sub
+End Class
